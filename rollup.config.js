59fe85bad21f975d8fafd5ad20c8e516079ac6ce
Rich Harris <richard.a.harris@gmail.com>
2017-04-12 12:49:01 -0400

    admin

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,11 @@ import buble from 'rollup-plugin-buble';
 
 export default {
 	entry: 'src/index.js',
-	plugins: [ buble() ],
+	plugins: [
+		buble({
+			target: { node: 4 }
+		})
+	],
 	targets: [
 		{ dest: 'dist/rollup-plugin-svelte.cjs.js', format: 'cjs' },
 		{ dest: 'dist/rollup-plugin-svelte.es.js', format: 'es' }
